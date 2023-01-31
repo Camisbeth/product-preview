f2b84ed364fcc9874e13e8d6b190eb2cca090acc
Cam <camilacore1230170@gmail.com>
2023-01-31 10:25:30 -0300

    implementing product preview

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/global.scss";
+import "./styles/variables.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="section">
+      <figure className="figure">
+        <img className="img" src="/images/image-product-desktop.jpg" />
+      </figure>
+      <section className="information">
+        <p className="perfume">PERFUME</p>
+        <h1 className="tittle">Gabrielle Essence Eau De Parfum</h1>
+        <p className="parrafo">
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <section className="prices">
+          <p className="price">$149.99</p>
+          <p className="second-price">$169.99</p>
+        </section>
+        <button className="button">
+          <img src="/images/icon-cart.svg" />
+          Add to Cart
+        </button>
+      </section>
+    </section>
   );
 }
 

900873bf6b3b28d521be762b29ad91d41b4d6826
Cam <camilacore1230170@gmail.com>
2023-03-28 09:27:28 -0300

    fixing margin-top

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ function App() {
       <figure className="figure">
         <img className="img" src="/images/image-product-desktop.jpg" />
       </figure>
+
       <section className="information">
         <p className="perfume">PERFUME</p>
+
         <h1 className="title">Gabrielle Essence Eau De Parfum</h1>
+
         <p className="parrafo">
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
+
         <section className="prices">
           <p className="price">$149.99</p>
           <p className="second-price">$169.99</p>
         </section>
+
         <button className="button">
           <img src="/images/icon-cart.svg" />
           Add to Cart
